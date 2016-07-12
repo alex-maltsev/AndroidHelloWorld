f3ef375d28eeb7e063d86d2a91bb24cec0e2e58a
alex-maltsev <alex.maltsev@gmail.com>
2016-07-11 21:33:07 -0700

    Implemented rating activity and its presentation from the main activity

--- app/src/main/java/com/tva/helloworld/MainActivity.java
+++ app/src/main/java/com/tva/helloworld/MainActivity.java
@@ -1,19 +1,30 @@
 package com.tva.helloworld;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RatingBar;
 
 public class MainActivity extends AppCompatActivity {
+    private RatingBar ratingBar;
+    private View colorView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ratingBar = (RatingBar)findViewById(R.id.ratingBar);
+        colorView = findViewById(R.id.colorView);
     }
 
     public void showRatingActivityClicked(View view) {
+        float numStars = ratingBar.getRating();
 
+        Intent intent = new Intent(this, RatingActivity.class);
+        intent.putExtra("NUMBER_OF_STARS", numStars);
+        startActivity(intent);
     }
 
     public void showColorActivityClicked(View view) {
---
+++ app/src/main/java/com/tva/helloworld/RatingActivity.java
@@ -0,0 +1,22 @@
+package com.tva.helloworld;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+/**
+ * Created by Alex Maltsev on 7/11/16.
+ */
+public class RatingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rating);
+
+        float numStars = getIntent().getFloatExtra("NUMBER_OF_STARS", 0); // Will default to 0, if value is not passed in
+
+        TextView ratingText = (TextView)findViewById(R.id.ratingText);
+        ratingText.setText(String.valueOf(numStars));
+    }
+}
